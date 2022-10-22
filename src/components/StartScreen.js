cec515d2f3e19bb06350229d212e135a85005a0d
carlosdevprog <edulima.bcb@hotmail.com>
2022-10-22 03:36:18 -0300

    envio do projeto

---
+++ src/components/StartScreen.js
@@ -0,0 +1,13 @@
+import './StartScreen.css'
+
+const StartScreen = ({startGame}) => {
+  return (
+    <div className='start' >
+        <h1>Secret Word</h1>
+        
+        <button onClick={startGame} >Começar o jogo</button>
+    </div>
+  )
+}
+
+export default StartScreen
