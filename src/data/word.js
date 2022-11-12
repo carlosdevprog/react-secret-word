d30371fb88e723abf787d0e5fc45f9b00def581d
carlosdevprog <edulima.bcb@hotmail.com>
2022-11-11 22:03:58 -0300

    Novas Atualizacoes

--- src/data/word.js
+++ src/data/word.js
@@ -1,9 +1,10 @@
 export const wordsList = {
-    carro: ['Motor', 'Porta', 'Capô', 'Pneu', 'Antena'],
-    fruta: ['Banana', 'Maça', 'Pêra', 'Mamão', 'Laranja'],
-    corpo: ['Braço', 'Perna', 'Cérebro', 'Pescoço', 'Olhos'],
-    computador: ['Mouse', 'Teclado', 'Monitor', 'Gabinete'],
-    programação: ['Linguagem', 'Framework', 'Javascript', 'React'],
-    alimento: ['Arroz', 'Feijão', 'Carne', 'Leite', 'Ovo'],
-    animal: ['Javali', 'Cachorro', 'Baleia', 'Rinoceronte']
+    
+    fruta: ['Banana', 'Maça', 'Pêra', 'Mamão', 'Laranja', 'Jaca', 'Kiwi', 'Morango', 'Jabuticaba', 'Uva', 'Abacaxi', 'Caju', 'Framboesa', 'Tangerina', 'Abacate'],    
+    comida: ['Arroz', 'Feijão', 'Carne', 'Leite', 'Ovo', 'Lasanha', 'Strogonoff', 'Pizza', 'Panqueca', 'Tapioca', 'Lentilha', 'Macarrão', 'Inhame', 'Macaxeira'],
+    animal: ['Javali', 'Cachorro', 'Baleia', 'Rinoceronte', 'Gato', 'Coelho', 'Avestruz', 'Garça', 'Carangueijo', 'Tubarão', 'Escorpião', 'Cavalo', 'Cobra', 'Rato', 'Tucano', 'Tamanduá'],
+    objeto: ['Copo', 'Faca', 'Garfo', 'Mesa', 'Cadeira', 'Tapete', 'Cama', 'Vaso'],
+    Qual_o_feminino_de_bode: ['Cabra'],
+    Qual_o_feminino_de_leoa: ['Leão']
+
 }
